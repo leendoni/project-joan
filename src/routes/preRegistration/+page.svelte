585859d26e41f3b3597f0a01c4bd69770df6a337
Lyndone Valenzuela <lynd.two@gmail.com>
2023-06-19 20:03:24 +0800

    preRegistration initial

--- src/routes/preRegistration/+page.svelte
+++ src/routes/preRegistration/+page.svelte
@@ -1,9 +1,5 @@
-<!-- FINALIZED -->
-
-<!-- #region scripts -->
 <script>
-	import About from '../blocks/landing/about.svelte';
-	//#region components
+    import PreRegistration from '../../blocks/external/preRegistration.svelte';
 	import {
 		Button,
 		Content,
@@ -11,30 +7,22 @@
 		HeaderUtilities,
 		SkipToContent
 	} from 'carbon-components-svelte';
-	//#endregion
-	//#region icons
-	import { Login, WatsonHealthRegistration } from 'carbon-icons-svelte';
-	//#endregion
-	//#region pictograms
-	//#endregion
+	import { Login, PageFirst } from 'carbon-icons-svelte';
 	//#region variables
 	let isSideNavOpen = false;
 	//#endregion
 </script>
 
-<!-- #endregion -->
-
-<!-- #region header -->
 <Header company="Project" platformName="JOAN" bind:isSideNavOpen href="https://project-joan.cloud">
 	<div class="text-white invisible lg:visible">for Colegio de San Juan de Dios Incorporated</div>
 	<HeaderUtilities>
 		<div class="flex">
 			<div class="flex lg:hidden">
-				<Button kind="secondary" icon={WatsonHealthRegistration} href="/preRegistration/"/>
+				<Button kind="secondary" icon={PageFirst} href="../"/>
 				<Button kind="primary" icon={Login} />
 			</div>
 			<div class="hidden lg:flex">
-				<Button kind="secondary" icon={WatsonHealthRegistration} href="/preRegistration/">Pre-Register</Button>
+				<Button kind="secondary" icon={PageFirst} href="../">Back to Landing Page</Button>
 				<Button kind="primary" icon={Login}>Login</Button>
 			</div>
 		</div>
@@ -43,10 +31,9 @@
 		<SkipToContent />
 	</svelte:fragment>
 </Header>
-<!-- #endregion -->
 
-<img id="#" class="object-cover h-96 w-full" src="images/assets/banner.jpg" alt="" />
+<div class="h-96 w-full"></div>
 
 <Content>
-	<About />
+    <PreRegistration />
 </Content>
